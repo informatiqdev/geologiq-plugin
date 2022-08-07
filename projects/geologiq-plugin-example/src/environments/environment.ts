da6db58e5a30211c03106cbec1e9e4633e45b2fc
sufian <sufian0207012@gmail.com>
2022-08-07 19:25:49 +0600

    use unity build version alpha 18

--- projects/geologiq-plugin-example/src/environments/environment.ts
+++ projects/geologiq-plugin-example/src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
       dataUrl: 'https://informatiqdev.blob.core.windows.net/unity/geologiq/[version]/geologiq.data.gz',
       frameworkUrl: 'https://informatiqdev.blob.core.windows.net/unity/geologiq/[version]/geologiq.framework.js.gz',
       codeUrl: 'https://informatiqdev.blob.core.windows.net/unity/geologiq/[version]/geologiq.wasm.gz',
-      productVersion: '4.0.0-a.13'
+      productVersion: '4.0.0-a.18'
     },
     services: {
       apiKey: 'las9812mnf329asjdsf812lkj'
